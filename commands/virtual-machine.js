a806613a006ae47346516d68a45abfeb9f37ad1f
Weili Dai <weili.dai@ebay.com>
2018-07-09 16:09:34 +0100

    Added checkIsVmRunning

--- commands/virtual-machine.js
+++ commands/virtual-machine.js
@@ -1,9 +1,25 @@
 import vmStatus from './vm-status';
 
+const checkIsVmRunning = (sleepInSec, vmName) => {
+    return new Promise((res) => {
+        const isVmRunning = vmStatus();
+        if (isVmRunning) {
+            setTimeout(() => {
+                res(checkIsVmRunning(sleepInSec, vmName));
+            }, sleepInSec);
+        } else {
+            res();
+        }
+    });
+};
+
 export function shutdownVM(
+    vmName,
     statusCheckFrequencyInSec = 1,
-    timeoutInSec = 180
+    timeoutInSec = 180,
     ) {
     console.log('VM shutdown', statusCheckFrequencyInSec, timeoutInSec);
-    vmStatus();
+
+    return checkIsVmRunning(statusCheckFrequencyInSec * 1000, vmName);
 }
+
--- commands/virtual-machine.test.js
+++ commands/virtual-machine.test.js
@@ -1,5 +1,7 @@
+import "babel-polyfill"
 import {shutdownVM} from './virtual-machine';
 import * as vmStatus from './vm-status';
+
 
 test('shutdownVM() calls vmStatus()', () => {
     vmStatus.default = jest.fn();
@@ -8,9 +10,11 @@ test('shutdownVM() calls vmStatus()', () => {
     expect(vmStatus.default.mock.calls.length).toBeGreaterThan(0);
 });
 
-test('shutdownVM() calls vmStatus() 10 times with timeout of 10 seconds', () => {
-    vmStatus.default = jest.fn();
-    shutdownVM(1, 10);
+test('shutdownVM() calls vmStatus() 10 times with timeout of 10 seconds', async () => {
+    const vmName = '';
+    const responses = [...Array(9).fill(true), false];
+    vmStatus.default = jest.fn(() => responses.shift());
+    await shutdownVM(vmName, 0.01, 10);
 
     expect(vmStatus.default.mock.calls.length).toBe(10);
 });
