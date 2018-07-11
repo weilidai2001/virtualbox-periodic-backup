5141d6d6bc23758322c80b54e0d1f90474ab4845
Weili Dai <weili.dai@ebay.com>
2018-07-11 15:49:24 +0100

    Added express server

---
+++ src/server/routes.js
@@ -0,0 +1,52 @@
+import express from 'express';
+const app = express();
+import logger from '../logger';
+import {
+    shutdownVmWithTimeout,
+    copyVm,
+    checkVmCopiedCorrectly,
+    deleteOldestVmOverLimit,
+    startVm,
+} from '../commands';
+
+import config from '../config';
+
+let currentStatus = 'Idle';
+
+app.get('/', (req, res) => {
+    res.json({
+        currentStatus
+    });
+});
+
+app.get('/backup', (req, res) => {
+    res.json({
+        status: 'backup instruction received'
+    });
+
+    (async function doBackup(){
+        currentStatus = 'Beginning backup';
+        const vmName = config.vmName;
+        const vmFileName = config.vmFileName;
+
+        currentStatus = 'Waiting for shutdown';
+        await shutdownVmWithTimeout(vmName);
+
+        currentStatus = 'Starting copy';
+        const newName = await copyVm(vmFileName);
+
+        currentStatus = 'Checking copy';
+        await checkVmCopiedCorrectly(vmFileName, newName);
+
+        currentStatus = 'Deleting old';
+        await deleteOldestVmOverLimit(vmName);
+
+        currentStatus = 'Restarting vm';
+        await startVm(vmName);
+
+        currentStatus = 'Idle';
+    })();
+});
+
+app.listen(3000, () => logger.info('Virtualbox backup listening on port 3000!'));
+
